9748f68d6e3feabe3eb816c1b1b90992fd7b053a
solomon <amsalusolomon12@gmail.com>
2023-03-22 17:42:36 +0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Article(models.Model):
+    title=models.CharField(max_length=200)
+    body=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from blog.models import Article
+
+# Create your views here.
+class list(generic.ListView):
+    model=Article
+    template_name='article_list.html'
+    
+    def get(self, request, *args, **kwargs):
+        
+
+        articles = Article.objects.all().order_by('-created_at')
+        context = {
+            'articles': articles
+        }
+        return render(request,'blog/article_list.html', context)
+
+
